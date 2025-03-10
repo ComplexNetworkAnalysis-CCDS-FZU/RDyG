66c36d002e33eedca713ba35d15ec786a40d60b5
ForzenString <964413011@qq.com>
2025-03-10 17:00:50 +0800

    ✨ 使得批次化迭代器产生可以统计历史交互信息矩阵

--- src/model/batchtifier.py
+++ src/model/batchtifier.py
@@ -2,6 +2,7 @@ import abc
 from typing import Generic, List, TypeVar
 
 from more_itertools import chunked, take
+import torch
 
 from src.payload.event import Event
 
@@ -21,6 +22,42 @@ class EvenStream(abc.ABC):
         return self
 
 
+class BatchContainer:
+    def __init__(self, batch: List[Event]):
+        self._events = batch
+
+    def __len__(self):
+        return len(self._events)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self._events.__next__()
+
+    def interactive_mat(self):
+        src_num = len(set(map(lambda x: x.src_node, self._events)))
+        dst_num = len(set(map(lambda x: x.dst_node, self._events)))
+        interactive_matrix = torch.zeros([src_num, dst_num])
+        src_node_mapping = {}
+        dst_node_mapping = {}
+
+        for event in self._events:
+            if event.src_node not in src_node_mapping.keys():
+                src_node_mapping[event.src_node] = len(src_node_mapping)
+
+            src_idx = src_node_mapping[event.src_node]
+
+            if event.dst_node not in dst_node_mapping.keys():
+                dst_node_mapping[event.dst_node] = len(dst_node_mapping)
+
+            dst_idx = src_node_mapping[event.src_node]
+
+            interactive_matrix[src_idx, dst_idx] += 1
+
+        return interactive_matrix, src_node_mapping, dst_node_mapping
+
+
 ES = TypeVar("ES", bound=EvenStream)
 
 
@@ -35,13 +72,13 @@ class Batchtifier(Generic[ES]):
 
         self.inner_iter = iter(chunked(event_stream, batch_size))
 
-    def __next__(self) -> List[Event]:
+    def __next__(self) -> BatchContainer:
         try:
             if self.provide_intro:
                 self.provide_intro = False
                 return self.intro
             else:
-                return next(self.inner_iter)
+                return BatchContainer(next(self.inner_iter))
         except StopIteration:
             raise
 
