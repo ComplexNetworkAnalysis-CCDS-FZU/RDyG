739d5562ba53e556bede9d26be7b0e8c12b457e0
ForzenString <964413011@qq.com>
2025-03-31 15:49:08 +0800

    ✨ 接入DyGFormer 的NeighborSampler

--- src/payload/event.py
+++ src/payload/event.py
@@ -29,3 +29,9 @@ class Event(BaseModel):
             )
             for d in data
         ]
+        
+    def __hash__(self) -> int:
+        return self.src_node.__hash__() + self.dst_node.__hash__() + self.ty.__hash__() + self.timestamp.__hash__() + self.event_id.__hash__() + self.label.__hash__()
+    
+    def __eq__(self, value) -> bool:
+        return self.src_node == value.src_node and self.dst_node ==value.dst_node and self.timestamp == value.timestamp and self.event_id == value.event_id and self.label == value.label and self.ty == value .ty
---
+++ test/test_model_node_wish_batchify.py
@@ -0,0 +1,50 @@
+
+from typing import List
+import unittest
+
+from src.payload.event import Event
+from src.payload.event_stream import EventStream
+from src.utils import batchtifier
+from src.utils.batchtifier.node_wish_event_batch import NodeWishEventBatchtifier
+
+class TestEvenStream(EventStream):
+    def __init__(self, *events: Event):
+        super().__init__()
+        self.events = events
+        self.idx = 0
+
+    def __next__(self):
+        if self.idx >= len(self.events):
+            raise StopIteration
+        value = self.events[self.idx]
+        self.idx += 1
+        return value
+
+    def __len__(self):
+        return len(self.events)
+
+
+class TestNodeWishBatcher(unittest.TestCase):
+    
+    def test_no_align_batch(self):
+        event_stream = TestEvenStream(
+            *Event.from_list(
+                (1,2),(1,3),(2,3),
+                (1,4),(2,4),(1,2)
+            )
+        )
+        
+        batchtifier = NodeWishEventBatchtifier[TestEvenStream](event_stream,2)
+        batch_iter = iter(batchtifier)
+        
+        batch1 = next(batch_iter)
+        self.assertEqual(6,len(batch1))
+        
+        batch2 = next(batch_iter)
+        self.assertEqual(6,len(batch2))
+        
+    
+    def test_align_batch(self):
+        pass
+    
+    
